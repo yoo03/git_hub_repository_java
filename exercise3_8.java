e9b30a20a06c675a12d8a4007ebfa5c8127cd9e3
yoohaneol <hanearl1013@gmail.com>
2022-04-21 20:33:12 +0900

    first commit

---
+++ exercise3_8.java
@@ -0,0 +1,25 @@
+class exercise3_8{
+    public static void main(String[] args) 
+    {
+        byte a=10;
+        byte b=20;
+        int c=a+b;
+
+        char ch='A';
+        ch+=2;
+
+        float f =3/2f;
+        long l=3000l*3000l*3000l;
+
+        float f2=0.1f;
+        double d=0.1;
+
+        boolean result = d!=f2;
+        
+        System.out.println("c="+c);
+        System.out.println("ch="+ch);
+        System.out.println("f="+f);
+        System.out.println("l="+l);
+        System.out.println("result="+result);
+    }
+}
