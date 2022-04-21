fe6d467b69e1e3ee847148fc145bf79abda8ee34
yoohaneol <hanearl1013@gmail.com>
2022-04-21 20:24:20 +0900

    first commit

---
+++ exercise3_2.java
@@ -0,0 +1,8 @@
+class exercise{
+    public static void main(String[] args) {
+        int numOfApples=123;
+        int sizeOfBucket=10;
+        int numOfBucket=(numOfApples+sizeOfBucket)/sizeOfBucket;
+        System.out.println("필요한 바구니의 수: "+ numOfBucket);
+    }
+}
