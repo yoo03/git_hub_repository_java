adb04142c1266009d2bdcdded3a2015ff154778f
yoohaneol <hanearl1013@gmail.com>
2022-04-21 22:46:29 +0900

    first commit

---
+++ exercise5_3.java
@@ -0,0 +1,12 @@
+class exercise5_3
+{
+    public static void main(String[] args) 
+    {
+    int[] arr ={10, 20, 30, 40, 50};
+    int sum=0;    
+        for(int i=0; i<5; i++){
+            sum+=arr[i];
+        }
+        System.out.println("sum="+sum);
+    }
+}
