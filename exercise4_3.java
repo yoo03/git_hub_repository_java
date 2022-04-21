0121103e1b33d46536ad28857d89b43f162a031b
yoohaneol <hanearl1013@gmail.com>
2022-04-21 22:39:22 +0900

    first commit

---
+++ exercise4_3.java
@@ -0,0 +1,14 @@
+class exercise4_3
+{
+    public static void main(String[] args)
+     {
+         int sum=0;
+         int value=0;
+    for(int i=1; i<=10; i++)
+     {
+      sum+=i;
+      value+=sum;
+     }   
+     System.out.print(value);  
+    }
+}
