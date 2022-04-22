c1549fea945ff3dc3b27ffdbe9e76999d4833d8c
yoohaneol <hanearl1013@gmail.com>
2022-04-23 08:59:30 +0900

    first commit

---
+++ exercise5_9.java
@@ -0,0 +1,36 @@
+class exercise5_9{
+    public static void main(String[] args) {
+        char[][] star = {
+            {'*', '*', ' ', ' ', ' '},
+            {'*', '*', ' ', ' ', ' '},
+            {'*', '*', '*', '*', '*'},
+            {'*', '*', '*', '*', '*'}
+    };
+
+    char[][] result = new char[star[0].length][star.length];
+
+    for (int i=0; i<star.length; i++) {
+        for (int j=0; j<star[i].length; j++) {
+            System.out.print(star[i][j]);
+    }
+    System.out.println();
+}
+
+System.out.println();
+
+for (int i=0; i<star.length; i++) {
+    for (int j=0; j<star[i].length; j++) {	
+int a = j;
+int b = star.length-1-i;
+result[a][b] = star[i][j];			
+    }
+}
+
+for (int i=0; i<result.length; i++) {
+    for (int j=0; j<result[i].length; j++) {
+        System.out.print(result[i][j]);
+            }
+        System.out.println();
+        }
+    }  
+}
