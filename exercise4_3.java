17a87a545a569b4acfd3e3621adc1ce2766a6444
yoohaneol <hanearl1013@gmail.com>
2022-04-23 21:20:41 +0900

    second commit

--- exercise4_3.java
+++ exercise4_3.java
@@ -2,13 +2,13 @@ class exercise4_3
 {
     public static void main(String[] args)
      {
-         int sum=0;
-         int value=0;
-    for(int i=1; i<=10; i++)
+         int sum=0;//sum을 초기화후 재정의
+         int value=0;//value를 초기화후 재정의
+    for(int i=1; i<=10; i++)//i는 1부터 시작해서 10까지 증가하고 그때까지 for문은 계속돈다
      {
-      sum+=i;
-      value+=sum;
+      sum+=i;//sum에 i가 계속해서 더해지고
+      value+=sum;//그 sum이 value에 계속해서 더해짐
      }   
-     System.out.print(value);  
+     System.out.print(value); //value값을 받고 그것을 출력
     }
-}
+}//이 프로그램은 1+(1+2)+(1+2+3)+~~~~~를 구하는 프로그램이다.
