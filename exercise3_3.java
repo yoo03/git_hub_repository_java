2da75c80aa9229f6f34a9e9443ab1def7a0c1f3d
yoohaneol <hanearl1013@gmail.com>
2022-04-23 18:45:51 +0900

    second commit

--- exercise3_3.java
+++ exercise3_3.java
@@ -2,7 +2,8 @@ class exercise3_3
 {
     public static void main(String[] args) 
     {
-    int num =10;
-    System.out.println(num==0?"0":num>0?"양수":"음수");
+    int num =10;//num을 초기화 후 정의
+    System.out.println(num==0?"0":num>0?"양수":"음수");/*삼항연산자를 사용해 num이 0이면 0출력 0보다 크면 양수 
+    0보다 작으면 음수*/
     }
 }
