6191367bc2e9438f6b00481cd34e0d2aa6c42667
yoohaneol <hanearl1013@gmail.com>
2022-04-23 21:38:57 +0900

    second commit

--- exercise4_8.java
+++ exercise4_8.java
@@ -1,14 +1,14 @@
 public class exercise4_8
 {
     public static void main(String[] args) {
-        int sum=0;
-        for(int x=0; x<=10; x++){
-            for(int y=0; y<=10;y++){
-                sum=2*x+4*y;
-                if(sum==10){
-                    System.out.printf("x=%d, y=%d\n", x, y);
+        int sum=0;//sum을 초기화후 재정의 
+        for(int x=0; x<=10; x++){// x가 0부터 10까지있고 for문은 그때까지 계속해서돈다.
+            for(int y=0; y<=10;y++){//y는 0부터 10까지있고 for문은 그때까지 계속돈다. 
+                sum=2*x+4*y;//sum은 x에 2를 곱하고 y에 4를 곱한것을 더한것이다.
+                if(sum==10){//sum이 10일때
+                    System.out.printf("x=%d, y=%d\n", x, y);//x와 y의 값을 받아서 출력한다
                 }
             }
         }
     }
-}
+}//이 코드는 방정식 2x+4y의 x,y의 해를 찾는 코드이다.
