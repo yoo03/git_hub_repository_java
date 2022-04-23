ffd7feb205bf78c70b87c36c0cc833cefff55d6b
yoohaneol <hanearl1013@gmail.com>
2022-04-23 18:42:07 +0900

    second commit

--- exercise3_2.java
+++ exercise3_2.java
@@ -1,8 +1,9 @@
 class exercise{
     public static void main(String[] args) {
-        int numOfApples=123;
-        int sizeOfBucket=10;
-        int numOfBucket=(numOfApples+sizeOfBucket)/sizeOfBucket;
-        System.out.println("필요한 바구니의 수: "+ numOfBucket);
+        int numOfApples=123;  //사과 개수
+        int sizeOfBucket=10;  //한 바구니에 들어갈 수 있는 수
+        int numOfBucket=(numOfApples+sizeOfBucket)/sizeOfBucket;/* 바구니의 최대 개수를 구하는 것이므로 사과개수에 
+        한 바구니에 들어갈수있는 개수를 더해서 넘치게 만든다음 나누어서 개수를 구한다. */
+        System.out.println("필요한 바구니의 수: "+ numOfBucket);//출력
     }
 }
