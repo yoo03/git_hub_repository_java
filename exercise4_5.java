a9405452dd3ff0a4b26dea4e8142a7ee914f104c
yoohaneol <hanearl1013@gmail.com>
2022-04-30 00:43:44 +0900

    first commit

---
+++ exercise4_5.java
@@ -0,0 +1,16 @@
+class exercise4_5
+{
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        while(i<=10){
+            j=0;
+            while(j<=i){
+            System.out.print("*");
+            j++;
+        }
+        System.out.println();
+        i++;
+    }   
+    }
+}
