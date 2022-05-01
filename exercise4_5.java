5e478354bdacaedefaa5743d43f491ad92d3b261
yoohaneol <hanearl1013@gmail.com>
2022-05-01 09:05:14 +0900

    second commit

--- exercise4_5.java
+++ exercise4_5.java
@@ -1,16 +1,16 @@
 class exercise4_5
 {
     public static void main(String[] args) {
-        int i = 0;
-        int j = 0;
-        while(i<=10){
-            j=0;
-            while(j<=i){
-            System.out.print("*");
-            j++;
+        int i = 0;//변수 i초기화
+        int j = 0;//변수 j초기화
+        while(i<=10){//while은 i가 10이하일때까지 계속돈다
+            j=0;//변수 j 0으로 다시 초기화
+            while(j<=i){//while은 j가 i이하일때까지 계속돈다
+            System.out.print("*");//*를 출력한다
+            j++;//j증가
         }
-        System.out.println();
-        i++;
+        System.out.println();//줄바꿈
+        i++;//i증가
     }   
     }
 }
