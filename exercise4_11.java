05cc152bf3f05894db8dca281003c0fb47ab6bd2
yoohaneol <hanearl1013@gmail.com>
2022-05-02 13:38:31 +0900

    third commit

--- exercise4_11.java
+++ exercise4_11.java
@@ -1,8 +1,8 @@
 public class exercise4_11{
 public static void main(String[] args) {
-    int num1=1;//num1을 초기화하고 재정의
-    int num2=1;//num2를 초기화하고 재정의
-    int num3=0;//num3를 초기화하고 재정의
+    int num1=1;//변수 num1을 선언하고 1로 초기화
+    int num2=1;//변수 num2를 선언하고 1로 초기화
+    int num3=0;//변수 num3를 선언하고 0으로 초기화
     System.out.print(num1+","+num2);//num1과 num2를 받아서 출력
 
     for (int i=0;i<8;i++){//i는 0부터 시작해서 7까지가고 갈동안 for은 계속해서 돈다
