301c6378099a502151d6ef975f47a21e40d3c3e4
yoohaneol <hanearl1013@gmail.com>
2022-05-02 13:36:01 +0900

    third commit

--- exercise4_8.java
+++ exercise4_8.java
@@ -1,7 +1,7 @@
 public class exercise4_8
 {
     public static void main(String[] args) {
-        int sum=0;//sum을 초기화후 재정의 
+        int sum=0;//변수sum을 선언 후 0으로 초기화  
         for(int x=0; x<=10; x++){// x가 0부터 10까지있고 for문은 그때까지 계속해서돈다.
             for(int y=0; y<=10;y++){//y는 0부터 10까지있고 for문은 그때까지 계속돈다. 
                 sum=2*x+4*y;//sum은 x에 2를 곱하고 y에 4를 곱한것을 더한것이다.
