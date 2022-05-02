1b435e95868aeaae2d6065a5814938a649344669
yoohaneol <hanearl1013@gmail.com>
2022-05-02 13:21:49 +0900

    third commit

--- exercise4_2.java
+++ exercise4_2.java
@@ -1,7 +1,7 @@
 class exercise4_2
 {
     public static void main(String[] args) {
-        int sum=0;//sum을 초기화후 재정의
+        int sum=0;//변수 sum을 선언하고 0으로 초기화
         for(int i=0; i<=20; i++)//i가 0부터 20이 될때까지 계속해서 이 for문은 돈다.
         {
             if(i%2!=0 && i%3!=0)//i를 2로나누었을때 나머지가 0이 아니거나 i를 3으로 나누었을때 나머지가 0이아닌경우를 둘다만족할때
