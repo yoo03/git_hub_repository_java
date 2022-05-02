e39d767ca82150dd8a34ba0fa38a8673bbba9602
yoohaneol <hanearl1013@gmail.com>
2022-05-02 13:40:39 +0900

    third commit

--- exercise4_12.java
+++ exercise4_12.java
@@ -2,7 +2,7 @@ class exercise4_12
 {
     public static void main(String[] args) 
     {
-        int multiply=0;//mutiply를 초기화후 재정의
+        int multiply=0;//변수 mutiply를 선언하고 0으로 초기화
         for(int a=2; a<=9; a++){//a는 2부터 시작하여 9까지가고 그동안 for문은 계속해서 돈다
             for(int b=1;b<=3;b++){//b는 1부터 시작하여 3까지가고 그동안 for문은 계속해서 돈다
                 multiply=a*b;//multiply는 a와 b의 곱이다
