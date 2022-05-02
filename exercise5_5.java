cd7693205f5f4ab8e14e1f9b5b6384c4fbc95617
yoohaneol <hanearl1013@gmail.com>
2022-05-02 13:55:54 +0900

    third commit

--- exercise5_5.java
+++ exercise5_5.java
@@ -1,11 +1,11 @@
 class exercise5_5{
     public static void main(String[] args) {
-        int[] ballArr={1,2,3,4,5,6,7,8,9};//ballArr배열에 대한 정의
+        int[] ballArr={1,2,3,4,5,6,7,8,9};//ballArr배열을 선언하고 {1,2,3,4,5,6,7,8,9}초기화
         int[] ball3= new int[3];//ball3의 주소들어가는 길이는 3
 
         for(int i=0; i<ballArr.length; i++){//i는 0부터 시작해서 ballArr의 길이만큼 증가하고 그동안 for문은 지속된다
             int j=(int)(Math.random()*ballArr.length);//난수생성, 범위는 ballArr의 길이만큼
-            int tmp =0;//값을 스위치 하기위한 임의의 변수 초기화및 정의
+            int tmp =0;//값을 스위치 하기위한 임의의 변수 tmp으로의 선언과 0으로 초기화
 
             tmp=ballArr[i];//ballArr[i]를 tmp에 적용
             ballArr[i]=ballArr[j];//ballArr[j]를 ballArr[i]에 적용
