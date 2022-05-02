9996204dfbab6283fc0bf63aba76026ef04690b2
yoohaneol <hanearl1013@gmail.com>
2022-05-02 13:48:15 +0900

    fourth commit

--- exercise5_3.java
+++ exercise5_3.java
@@ -2,8 +2,8 @@ class exercise5_3
 {
     public static void main(String[] args) 
     {
-    int[] arr ={10, 20, 30, 40, 50};//일차원 배열의 정의
-    int sum=0;//sum을 초기화후 재정의
+    int[] arr ={10, 20, 30, 40, 50};//일차원 배열 arr의 선언 후 {10, 20, 30, 40, 50}으로 초기화
+    int sum=0;//변수 sum을 선언후 0으로 초기화
         for(int i=0; i<5; i++){//i는 0부터 4까지 증가하고 그동안 for문은 계속해서 돌아간다
             sum+=arr[i];//배열안에 있는 값이 sum에 계속해서 더해진다
         }
