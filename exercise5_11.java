5a544398cee0f1f65e74eecd1a296972b15067c4
yoohaneol <hanearl1013@gmail.com>
2022-05-08 13:22:46 +0900

    second commit

--- exercise5_11.java
+++ exercise5_11.java
@@ -7,24 +7,24 @@ class exercise5_11
 			{30, 30, 30},
 			{40, 40, 40},
 			{50, 50, 50}
-		};
+		};// score에 대한 배열 선언 및 초기화
 		
 		int[][] result = new int[score.length+1][score[0].length+1];
 		
-		for (int i=0; i<score.length; i++) {
-			for (int j=0; j<score[i].length; j++) {
-				result[i][j] = score[i][j];
-				result[i][result[i].length-1] += score[i][j];
-				result[result.length-1][j] += score[i][j];
+		for (int i=0; i<score.length; i++) {// i는 0부터 시작해서 배열score의 길이만큼 증가하고 그동안 for문은 계속해서 돈다
+			for (int j=0; j<score[i].length; j++) {// j는 0부터 시작해서 배열score[i]의 길이만큼 증가하고 그동안 for문은 계속해서 돈다
+				result[i][j] = score[i][j];// score의 값을 result에 대입
+				result[i][result[i].length-1] += score[i][j];// score의 값을 length에서 1뺀값에 대입 왜냐하면 배열이 1이 더크기 때문에
+				result[result.length-1][j] += score[i][j];// 위와동일하게 j값을 대입
 			}
-			result[result.length-1][result[i].length-1] += result[i][result[i].length-1];
+			result[result.length-1][result[i].length-1] += result[i][result[i].length-1];// result에 합을더해 총합을 저장
 		}
 		
-		for (int i=0; i<result.length; i++) {
-			for (int j=0; j<result[i].length; j++) {
-				System.out.printf("%4d", result[i][j]);
+		for (int i=0; i<result.length; i++) {//i는 0부터 시작해서 배열result의 길이만큼 증가하고 그동안 for문은 계속해서 돈다
+			for (int j=0; j<result[i].length; j++) {//j는 0부터 시작해서 배열result의 길이만큼 증가하고 그동안 for문은 계속해서 돈다
+				System.out.printf("%4d", result[i][j]);// 출력
 			}
-			System.out.println();
+			System.out.println();// 줄바꿈
 		}
 	}
     }
