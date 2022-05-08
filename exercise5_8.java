80c18e8c5b0f5fc212d4509258ad2cb89938d4f5
yoohaneol <hanearl1013@gmail.com>
2022-05-08 10:05:52 +0900

    fifth commit

--- exercise5_8.java
+++ exercise5_8.java
@@ -4,7 +4,7 @@ class execise5_8{
 		int[] counter = new int[4];//int배열에 대한 번지수, 총 4칸
 		
 		for (int i=0; i<answer.length; i++) {//i는 0부터 answer배열의 길이까지 증가하며 그동안 for문은 계속해서 돌아간다
-			counter[answer[i]-1]++;//
+			counter[answer[i]-1]++;//counter배열안의 값을 계속바꾸어 별의 개수를 지정해주는 코드
 		}
 		
 		for (int i=0; i<counter.length; i++) {//i는 0부터 시작하여 counter배열의 길이까지 증가하며 그동안 for문은 계속해서 돌아간다
@@ -12,7 +12,7 @@ class execise5_8{
 			for (int j=0; j<counter[i]; j++) {//j는 0부터 시작하여 counter배열의 길이까지 증가하며 그동안 for문은 계속해서 돌아간다
 				System.out.print("*");//*출력
 			}
-			System.out.println();//출력
+			System.out.println();//줄바꿈 출력
 		}
 }
-}//
+}// 배열에 담긴 데이터를 읽고 숫자를 세어서 *을 찍어서 그래프를 그리는 코드
